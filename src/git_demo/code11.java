051ded64aa8ddf8aee2786318fe2bc7534a9e201
Lancelot2515 <812949176@qq.com>
2020-09-22 16:34:26 +0800

    课本第一章第二章例题代码

---
+++ src/git_demo/code11.java
@@ -0,0 +1,16 @@
+package git_demo;
+
+import java.util.Scanner;
+
+public class code11 {
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter a purchase amount: ");
+		double purchaseAmount = input.nextDouble();
+		double tax = purchaseAmount * 0.06;
+		
+		System.out.println("Sales tax is $"+(int)(tax*100)/100.00);
+
+	}
+}
---
+++ src/git_demo/code2.java
@@ -0,0 +1,8 @@
+package git_demo;
+
+public class code2 {
+	public static void main(String[] args) {
+		System.out.println("(10.5+2*3)/(45-3.5)=");
+		System.out.println((10.5+2*3)/(45-3.5));
+	}
+}
