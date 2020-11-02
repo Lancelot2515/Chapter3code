0ffff1f1f2d9868c6d22c88b9c8128c6cb475e1c
Lancelot2515 <812949176@qq.com>
2020-11-02 21:54:31 +0800

    第九章例题作业

---
+++ src/git_demo/第九章例题/CircleWithPrivateDataFields.java
@@ -0,0 +1,26 @@
+package git_demo.第九章例题;
+
+public class CircleWithPrivateDataFields {
+	private double radius=1;
+	private static int numberOfObjects=0;
+	public  CircleWithPrivateDataFields() {
+		numberOfObjects++;
+	}
+	public CircleWithPrivateDataFields(double newradius) {
+		radius=newradius;
+		
+	}
+	public double getRadius() {
+		return radius;
+	}
+	public void setRadius(double newradius) {
+		radius=(newradius>=0)?newradius:0;
+	}
+	public static int getNumberOfObjects() {
+		return numberOfObjects;
+	}
+	public double getArea() {
+		return radius*radius*Math.PI;
+	}
+}
+
---
+++ src/git_demo/第九章例题/TestSimpleCircle.java
@@ -0,0 +1,32 @@
+package git_demo.第九章例题;
+
+public class TestSimpleCircle {
+	public static void main(String[] args) {
+		Simplecircle circle1 = new Simplecircle();
+		System.out.println("The area of the circle of radius"+circle1.radius+" is "+circle1.getArea());
+		Simplecircle circle2 = new Simplecircle(25);
+		System.out.println("The area of the circle of radius"+circle2.radius+" is "+circle2.getArea());
+		Simplecircle circle3 = new Simplecircle(125);
+		System.out.println("The area of the circle of radius"+circle3.radius+" is "+circle3.getArea());
+		circle2.radius=100;
+		System.out.println("The area of the circle of radius"+circle2.radius+" is "+circle2.getArea());
+	}
+}
+class Simplecircle{
+	double radius;
+	public Simplecircle() {
+		radius=1;
+	}
+	Simplecircle(double newRadius){
+		radius=newRadius;
+	}
+	double getArea() {
+		return radius*radius*Math.PI;
+	}
+	double getPerimeter() {
+		return 2*radius*Math.PI;
+	}
+	void setRadius(double newRadius) {
+		radius=newRadius;
+	}
+}
